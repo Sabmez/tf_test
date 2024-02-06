b28e7106428a8db0337a366f9ae9ea17aaa2a066
Sabmez <Sabrina.meziani19@gmail.com>
2024-02-06 14:43:01 +0100

    testf

--- main.tf
+++ main.tf
@@ -8,15 +8,5 @@ resource "azurerm_resource_group" "Sabrina_RG" {
   location = "francecentral"
 }
 
-resource "azurerm_storage_account" "Sabrina_store" {
-  name                     = "sab-store"
-  resource_group_name      = azurerm_resource_group.Sabrina_RG.name
-  location                 = azurerm_resource_group.Sabrina_RG.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
 
-  network_rules {
-    default_action = "Deny"
-    ip_rules       = ["23.45.1.0/30"]
-  }
-}
+  
